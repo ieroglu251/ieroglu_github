6853763c30f366e335c4e9ab1ca7e9444ad1edf8
ieroglu251 <ieroglu251@gmail.com>
2019-07-18 10:26:12 -0500

    data driven from excel comoleted.

--- src/test/java/Udemy/dataDriven.java
+++ src/test/java/Udemy/dataDriven.java
@@ -1,14 +1,20 @@
 package Udemy;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.NumberToTextConverter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class dataDriven {
 
-    public ArrayList<String> getData(String testcaseName) throws IOException {
+    public static ArrayList<String> getData(String testcaseName) throws IOException {
         ArrayList<String> a = new ArrayList<String>();
 
         FileInputStream fis = new FileInputStream("C:\\Users\\Ismail Eroglu\\Others\\Desktop\\seleniumTest.xlsx");
@@ -16,8 +22,61 @@ public class dataDriven {
         XSSFWorkbook workbook = new XSSFWorkbook(fis);
 
         int sheets = workbook.getNumberOfSheets();
+        System.out.println( "Number of sheets: "+sheets );
+
+        for(int i = 0; i < sheets; i++){
+            if(workbook.getSheetName(i).equalsIgnoreCase("dataType")   ){
+                XSSFSheet sheet = workbook.getSheetAt(i);
+
+                Iterator<Row> rows = sheet.iterator();
+                Row firstrow = rows.next();
+
+                Iterator<Cell> ce = firstrow.cellIterator();
+
+                int k  = 0;
+                int coloumn = 0;
+                while(ce.hasNext()){
+                    Cell value = ce.next();
+
+                    if(value.getStringCellValue().equalsIgnoreCase("dataType")){
+                        coloumn = k;
+
+                    }
+                    k++;
+                }
+                System.out.println( coloumn );
+
+                while (rows.hasNext()){
+                    Row r = rows.next();
+
+                    if(r.getCell(coloumn).getStringCellValue().equalsIgnoreCase(testcaseName)   ){
+                        Iterator<Cell> cv = r.cellIterator();
+                        while(cv.hasNext()){
+                            Cell c= cv.next();
+                            if(c.getCellTypeEnum()== CellType.STRING){
+                                a.add(c.getStringCellValue() );
+                            }else{
+                                a.add(NumberToTextConverter.toText(c.getNumericCellValue()));
+                            }
+
+                        }
+                    }
+                }
+            }
+
+        }
 
         return a;
+    }
+
+    public static void main(String[] args) {
+
+        try{
+            getData("dataType");
+        }catch (Exception e){
+            System.out.println("something wrong");
+        }
+
     }
 
 }
